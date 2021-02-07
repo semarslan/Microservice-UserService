56e1468f74c8db240962ab4602eaabdb111c5ab9
semarslan <sema.arslan@bil.omu.edu.tr>
2021-02-07 10:59:33 +0300

    user service completed

--- src/main/java/com/semarslan/user/service/impl/UserServiceImpl.java
+++ src/main/java/com/semarslan/user/service/impl/UserServiceImpl.java
@@ -34,7 +34,7 @@ public class UserServiceImpl implements UserService {
 
         User user = userRepository.findByUserId(userId);
 
-        Department department = restTemplate.getForObject("http://localhost:9001/departments/" + user.getDepartmentId(), Department.class);
+        Department department = restTemplate.getForObject("http://DEPARTMENT-SERVICE/departments/" + user.getDepartmentId(), Department.class);
 
         vo.setUser(user);
         vo.setDepartment(department);
